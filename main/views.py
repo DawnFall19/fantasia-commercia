ba4ddaa6eb0af03ef8bbd3a0f82ae2b3a50b2c69
Michael Ignasius <michael.ignasius@ui.ac.id>
2024-09-09 15:23:33 +0700

    Memperbaiki views.py dan main.html

--- main/views.py
+++ main/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306259950',
         'name': 'Michael Ignasius',
         'class': 'PBP E'
     }
